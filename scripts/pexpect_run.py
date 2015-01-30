2b76a866521c6dca62bb2f33e5f892f73016722f
Evan Sarmiento <esarmien@g.harvard.edu>
2015-01-30 10:54:28 -0500

    Pexpect run wrapper

--- scripts/pexpect_run.py
+++ scripts/pexpect_run.py
@@ -2,4 +2,12 @@
 import sys
 import pexpect
 
-pexpect.run(' '.sys.argv[1:])
+_logfile = sys.argv[1]
+
+try:
+  with open(_logfile, 'w+') as _logfile_fd:
+    pexpect.run(' '.join(sys.argv[2:]), logfile=_logfile_fd)
+except:
+  sys.exit(1)
+
+sys.exit(0)
