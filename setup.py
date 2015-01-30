4574a5f81eb08b0f41559e4ae80a47d78887d73d
Evan Sarmiento <esarmien@g.harvard.edu>
2015-01-30 14:02:45 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='rce-interactive-tools',
+      version='1.0',
+      description='HMDC utilities and scripts to submit condor jobs.',
+      url='https://github.com/hmdc/nx4_selenium_test',
+      author='Evan Sarmiento',
+      author_email='https://github.com/hmdc/rce-interactive-tools',
+      license='MIT',
+      packages=['hmdccondor', 'rceapp'],
+      requires=['pexpect', 'tabulate', 'progressbar'],
+      data_files=[('/usr/local/HMDC/etc', ['rceapp.yml'])],
+      scripts=[
+        'scripts/rce_submit.py',
+        'scripts/HMDC_interactive_prepare_job.py',
+        'scripts/HMDC_job_wrapper.py',
+        'scripts/pexpect_run.py'
+        ]
+)
