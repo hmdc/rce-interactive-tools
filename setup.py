8646755f747e6d2e5f74792edf37607b96d1452d
Evan Sarmiento <esarmien@g.harvard.edu>
2015-03-16 12:07:51 -0400

    Trying this to fix permissions

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+import distutils
 from os import chmod
 from distutils.core import setup
+
+class my_install(distutils.command.install_lib.install_lib):
+  def run(self):
+    distutils.command.install_lib.install_lib.run(self)
+    for fn in self.get_outputs():
+      if <this is one of the binaries I want to be executable>:
+        # copied from distutils source - make the binaries executable
+        mode = ((os.stat(fn).st_mode) | 0555) & 0755
+        distutils.log.info("changing mode of %s to %o", fn, mode)
+        os.chmod(fn, mode)
 
 setup(name='rce-interactive-tools',
       version='1.0',
@@ -8,6 +19,7 @@ setup(name='rce-interactive-tools',
       author='Evan Sarmiento',
       author_email='https://github.com/hmdc/rce-interactive-tools',
       license='MIT',
+      cmdclass={'install': my_install}
       packages=['hmdccondor', 'rceapp'],
       requires=['pexpect', 'tabulate', 'progressbar', 'pyyaml'],
       data_files=[('/etc', ['rceapp.yml.example'])],
