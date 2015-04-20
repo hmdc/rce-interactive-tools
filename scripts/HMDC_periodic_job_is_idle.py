298f6a76e5d8da50bccdf965b08f7bdaa959613b
Evan Sarmiento <esarmien@g.harvard.edu>
2015-04-20 15:16:21 -0400

    Add logging

--- scripts/HMDC_periodic_job_is_idle.py
+++ scripts/HMDC_periodic_job_is_idle.py
@@ -2,24 +2,40 @@
 import sys
 import classad
 import htcondor
+import logging
+import logging.handlers
 from hmdccondor import HMDCCondor
 # Import classad
 
+# Setup logging
+log = logging.getLogger(__name__)
+
+log.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+
+formatter = logging.Formatter('%(module)s.%(funcName)s: %(message)s')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
 
 def update_job(schedd, clusterid, jobid, is_job_idle):
   try:
     q_classad = schedd.query('ClusterId =?= {0}'.format(clusterid))[0]
   except:
+    log.critical('Job {0}: Could not find ClassAd'.format(jobid))
     return 0
 
-  print '[DEBUG] in update_job, found ClassAd for {0}'.format(jobid)
+  log.info('Job {0}: Found ClassAd.'.format(jobid))
 
   try:
     current_time = int(q_classad['CurrentTime'].eval())
   except:
+    log.critical(
+        'Job {0}: Could not evaluate CurrentTime'.format(jobid)
+        )
     return 0
 
-  print '[DEBUG] {0} CurrentTime = {1}'.format(jobid, current_time)
+  log.info('Job {0}: CurrentTime = {1}'.format(jobid, current_time))
 
   try:
     last_check_time = int(q_classad['LastIdleCheckTime'])
@@ -30,12 +46,12 @@ def update_job(schedd, clusterid, jobid, is_job_idle):
   except:
     return 0
 
-  print '[DEBUG]\
- {0},last_check_time={1},current_time={2},idle_time={3}'.format(jobid,
-      last_check_time,
-      current_time,
-      idle_time)
- 
+  log.info('Job {0}: \
+last_check_time={1},current_time={2},idle_time={3}'.format(jobid,
+        last_check_time,
+        current_time,
+        idle_time))
+
   if is_job_idle:
     differend = current_time - last_check_time
     idle_time += differend if last_check_time > 0 else 0
@@ -45,19 +61,23 @@ def update_job(schedd, clusterid, jobid, is_job_idle):
   try:
     schedd.edit([jobid], 'LastIdleCheckTime', str(current_time))
     schedd.edit([jobid], 'JobCpuIdleTime', str(idle_time))
-    print '[DEBUG] SET {0} : LastIdleCheckTime={1},JobCpuIdleTime={2}'.format(
-      jobid,
-      current_time,
-      idle_time,
-      )
   except:
+    log.critical('Job {0}: Unable to edit ClassAd in queue'.format(
+      jobid))
     return 0
+
+  log.info('Job {0}: LastIdleCheckTime={1}, JobCpuIdleTime={2}'.format(
+    jobid,
+    current_time,
+    idle_time))
 
   return 0
 
 
 def main():
+  
   hmdc_condor = HMDCCondor()
+
   schedd = hmdc_condor._interactive_schedd
   int_collect = hmdc_condor._collector_int
 
@@ -65,12 +85,13 @@ def main():
 
   # If the job isn't currently running, we don't care.
   if job_classad['JobStatus'] != 2:
+    job.info('Job is no longer running, exiting.')
     return 0
 
   clusterid = job_classad['ClusterId']
   procid = job_classad['ProcId']
 
-  print '[DEBUG] {0}.{1} is running.'.format(clusterid, procid)
+  log.info ('Job {0}.{1}: Running.'.format(clusterid, procid))
 
   jobid = "{0}.{1}".format(
       str(clusterid),
@@ -81,9 +102,10 @@ def main():
         'JobId =?= "{0}"'.format(jobid),
         ['JobCpuIsIdle'])[0]['JobCpuIsIdle'].eval()
 
-    print '[DEBUG] {0} is idle? {1}'.format(jobid, is_job_idle)
+    log.info('Job {0}: Idle? {1}'.format(jobid, is_job_idle))
 
   except:
+    log.info('Job {0}: Unable to evaluate JobCpuIsIdle')
     return 0
 
   return update_job(schedd,
