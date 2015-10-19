22594a8dbb0bba3a613c53c235b8d761293f5a21
Evan Sarmiento <esarmien@g.harvard.edu>
2015-10-19 12:29:13 -0400

    Fix HMDCLog.py

--- hmdccondor/HMDCLog.py
+++ hmdccondor/HMDCLog.py
@@ -2,13 +2,18 @@ import logging
 
 class HMDCLog:
   def __init__(self, name, level=logging.DEBUG):
-    self.log = __update_settings(logging.getLogger(name))
+    self.log = __update_settings__(logging.getLogger(name))
+
+  def __update_handler__(self, handler):
+    handler.setFormatter(logging.Formatter(
+      '%(module)s.%(funcName)s: %(message)s'))
+    return handler
 
   def __update_settings__(self, log):
     log.setLevel(level)
-    log.addHandler((logging.handlers.SysLogHandler(address='/dev/log').
-      setFormatter(logging.Formatter(
-        '%(module)s.%(funcName)s: %(message)s'))))
+    log.addHandler(
+      self.__update_handler__(logging.handlers.SysLogHandler(
+	address='/dev/log')))
     return log
 
 
