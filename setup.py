0ca7a0a8b3920b0dbc0b64db9ece162d9fc4a483
Evan Sarmiento <esarmien@g.harvard.edu>
2015-10-23 13:22:39 -0400

    Fix gauss filepath

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import chmod
 from distutils.core import setup
 
 setup(name='rce-interactive-tools',
-      version='1.5.11',
+      version='1.5.12',
       description='HMDC utilities and scripts to submit condor jobs.',
       url='https://github.com/hmdc/rce-interactive-tools',
       author='Evan Sarmiento',
