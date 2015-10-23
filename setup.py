f5d6094f0856ef3f7b4e8b807450fa4bf88d7a72
Evan Sarmiento <esarmien@g.harvard.edu>
2015-10-23 13:18:15 -0400

    * Add Mathematica 10.3 and SAS 9.4; trim Mathematica 10.0.1 * Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import chmod
 from distutils.core import setup
 
 setup(name='rce-interactive-tools',
-      version='1.5.10',
+      version='1.5.11',
       description='HMDC utilities and scripts to submit condor jobs.',
       url='https://github.com/hmdc/rce-interactive-tools',
       author='Evan Sarmiento',
