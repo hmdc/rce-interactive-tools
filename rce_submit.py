7f5d732daad723d1019ce559dc3a008c13456a6b
Evan Sarmiento <esarmien@g.harvard.edu>
2015-10-26 08:48:58 -0400

    Add beginning files for GUI

---
+++ rce_submit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2.6
+from hmdccondor.HMDCRceSubmitClient import HMDCRceSubmitClient
+
+if __name__ == '__main__':
+  exit(HMDCRceSubmitClient().run())
---
+++ test.py
@@ -0,0 +1,10 @@
+import logging
+import logging.handlers
+
+try:
+ 1 / 0
+except Exception as e:
+ pass
+
+print 10 / 5
+ 
