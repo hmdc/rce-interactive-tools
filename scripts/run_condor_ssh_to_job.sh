2afcf4a84a967ded51679275644ac53e8948ac07
Evan Sarmiento <esarmien@g.harvard.edu>
2017-01-17 10:59:41 -0500

    Fix wrapper script for XPRA 1.0.1

--- scripts/run_condor_ssh_to_job.sh
+++ scripts/run_condor_ssh_to_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is because XPRA is passing -p and -T which we aren't interested
 # in. Hacky! Help! Temporary only!
-/usr/bin/condor_ssh_to_job "${1}" "${2}" "${@:6}"
+/usr/bin/condor_ssh_to_job ${1} "${2}" ${4} "${@:5}"
