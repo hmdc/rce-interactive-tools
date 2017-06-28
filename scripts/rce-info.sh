e5e3c436c1a369c1e9904ebb61f413bbfcc23a93
William Horka <whorka@hmdc.harvard.edu>
2017-06-28 12:25:18 -0400

    fix rce-info.sh to not report servers available unless Start=true, and also extend entitlement-checking logic to NSAPH

--- scripts/rce-info.sh
+++ scripts/rce-info.sh
@@ -51,9 +51,28 @@ shift $(($OPTIND - 1))
 CONDOR_CONFIG_VAL=$(which condor_config_val)
 FIGLET=$(which figlet 2>/dev/null || echo /usr/bin/figlet)
 
-# Only include ksg machines if user has kennedy entitlement
-_CONSTRAINT='RemoteOwner =?= undefined'
-CONSTRAINT="$(echo ${_CONDOR_ENTITLEMENTS}|grep kennedy >/dev/null 2>&1 && echo "${_CONSTRAINT}" || echo "${_CONSTRAINT} && ! regexp(\"ksg\", Machine) && ! regexp (\"edlabs\", Machine) ")"
+# Only include ksg/edlabs/nsaph machines if user has matching entitlement
+#  (HMDC profile sets _CONDOR_ENTITLEMENTS at login)
+CONDOR_ENTITLEMENTS=`echo ${_CONDOR_ENTITLEMENTS} |sed -e 's/[,"]/ /g'`
+_CONSTRAINT='regexp("^slot1@",Name) && Start == true && RemoteOwner =?= undefined'
+_NOKSG=' && ! regexp("ksg", Machine)'
+_NOEDLABS=' && ! regexp ("edlabs", Machine)'
+_NONSAPH=' && ! regexp ("nsaph", Machine)'
+for title in ${CONDOR_ENTITLEMENTS}; do
+    echo "$title"
+    case "$title" in
+        kennedy)
+            _NOKSG=''
+        ;;
+        edlabs)
+            _NOEDLABS=''
+        ;;
+        nsaph)
+            _NONSAPH=''
+        ;;
+    esac
+done
+CONSTRAINT="${_CONSTRAINT} ${_NOKSG} ${_NOEDLABS} ${_NONSAPH}"
 
 # Main
 
@@ -65,11 +84,14 @@ cat <<EOF|fold -w 70 -s
 This displays the amount of cpu(s) and memory in gigabytes available for use on each host in our cluster. If you're submitting an RCE powered job, make sure that the amount of memory and cpu(s) requested fall within available resources on at least one of the cluster hosts listed below. Otherwise, your job will remain in the queue until enough resources become available.
 
 EOF
-(echo CpusAvailable MemoryAvailable\(gb\) Machine; condor_status -constraint "regexp(\"^slot1@\",Name) && ${CONSTRAINT}" -format "%d " Cpus -format "%d " Memory/1024 -format "%s\n" "regexps(\"^slot1@(.*)\", Name, \"\\1\")" |sort -n -r)|column -t
+(echo CpusAvailable MemoryAvailable\(gb\) Machine; condor_status -constraint "${CONSTRAINT}" -format "%d " Cpus -format "%d " Memory/1024 -format "%s\n" "regexps(\"^slot1@(.*)\", Name, \"\\1\")" |sort -n -r)|column -t
     ;;
     used)
       ${FIGLET} -f small 'RCE Cluster' 2> /dev/null|| echo "==== RCE Cluster ===="
       /usr/bin/condor_q -global -currentrun -constraint '( JobStatus == 2)' -format '%s ' User -format '%d ' RequestCpus -format '%d \n' RequestMemory |perl -e 'while (<>) { @f = split(/\s+/, $_); $sum{$f[0]}{cpu} += $f[1]; $sum{$f[0]}{mem} += $f[2]; } printf( "%15s %4s %s\n", "USER", "CPUS", "MEM_GB" ); foreach $u ( sort(keys(%sum)) ) { $us = $u; $us =~ s/\@hmdc\.harvard\.edu//; printf( "%15s %4d %d\n", $us, $sum{$u}{cpu}, $sum{$u}{mem} / 1024 ); }'
     ;;
+    *)
+        echo "Unrecognized type: $TYPE"
+        ;;
 esac
 
