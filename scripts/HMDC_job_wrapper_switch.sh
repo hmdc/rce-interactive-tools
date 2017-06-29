5c99dc0d049a78d8b927cee1459959f4592d5fc6
William Horka <whorka@hmdc.harvard.edu>
2017-06-29 12:48:55 -0400

    fix HMDC_job_wrapper_switch.sh to properly quote command-line arguments (cherry picked from commit af73b133340447e84db84a70f053352fb40b12af)

--- scripts/HMDC_job_wrapper_switch.sh
+++ scripts/HMDC_job_wrapper_switch.sh
@@ -2,7 +2,10 @@
 
 # This is a temporary wrapper.
 
+# Select the new/BETA wrapper for jobs submitted through rce_submit.py
+# Or select the old/STABLE wrapper for all other jobs
+
 HMDC_BETA_WRAPPER=$(/usr/bin/condor_config_val BETA_JOB_WRAPPER)
 HMDC_STABLE_WRAPPER=$(/usr/bin/condor_config_val STABLE_JOB_WRAPPER)
 
-(grep HMDCNewSubmit ${TEMP}/.job.ad > /dev/null 2>&1) && exec $HMDC_BETA_WRAPPER $@ || exec $HMDC_STABLE_WRAPPER $@
+(grep HMDCNewSubmit ${TEMP}/.job.ad > /dev/null 2>&1) && exec $HMDC_BETA_WRAPPER ${1+"$@"} || exec $HMDC_STABLE_WRAPPER ${1+"$@"}
